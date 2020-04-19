0e94b76111a77101f3c79029e4427c7c27eb453a
unknown <javadesmesh@gmail.com>
2020-04-19 14:26:22 +0430

    add new feauture ,getproduct,consumeProduct,launch with new method

--- cafe/src/main/java/com/teamtext/cafe/CafeBazaar.java
+++ cafe/src/main/java/com/teamtext/cafe/CafeBazaar.java
@@ -8,33 +8,35 @@ import android.util.Log;
 
 import com.teamtext.cafe.util.IabHelper;
 import com.teamtext.cafe.util.IabResult;
+import com.teamtext.cafe.util.Inventory;
 import com.teamtext.cafe.util.Purchase;
+
+import java.util.List;
 
 public class CafeBazaar implements Parcelable {
 
     private Activity activity;
     private boolean isSetup = false;
     private IabHelper iabHelper;
-    private CafeBazzarInterface cafeBazzarInterface;
+
     public CafeBazaar(Activity activity, String base64) {
         this.activity = activity;
         iabHelper = new IabHelper(activity, base64);
         try {
             setup();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public CafeBazaar(Activity activity, String base64, CafeBazzarInterface cafeBazzarInterface) {
+
+    public CafeBazaar(Activity activity, String base64, CafeBazaarInterface cafeBazaarInterface) {
         this.activity = activity;
-        this.cafeBazzarInterface=cafeBazzarInterface;
-        activity=new Activity();
+        activity = new Activity();
         iabHelper = new IabHelper(activity, base64);
         try {
             setup();
         } catch (Exception e) {
-            cafeBazzarInterface.ErrorSetupIabHelper(e);
+            cafeBazaarInterface.ErrorSetupIabHelper(e);
             e.printStackTrace();
         }
     }
@@ -55,16 +57,13 @@ public class CafeBazaar implements Parcelable {
         }
     };
 
-    private void setup() throws Exception
-    {
+    private void setup() throws Exception {
         iabHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
             public void onIabSetupFinished(IabResult result) {
-                Log.d("TAG", "Setup finished.");
-
                 if (!result.isSuccess()) {
-                    // Oh noes, there was a problem.
-                    Log.d("TAG", "Problem setting up In-app Billing: " + result);
+                    Log.d("TAG", "Problem setting up CafeBazaar: " + result);
                 } else {
+                    Log.d("TAG", "Setup finished CafeBazaar");
                     isSetup = true;
                 }
                 // Hooray, IAB is fully set up!
@@ -73,61 +72,102 @@ public class CafeBazaar implements Parcelable {
         });
     }
 
-    public void destroy() {
+    public void OnDestroyCafeBazaar() {
         if (iabHelper != null)
             iabHelper.dispose();
         iabHelper = null;
 
     }
 
+    public void getDetailProduct(boolean showDetail, List<String> products, final OnGetProductDetailFinished detailFinished) {
+        iabHelper.queryInventoryAsync(showDetail, products, new IabHelper.QueryInventoryFinishedListener() {
+            @Override
+            public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+                detailFinished.OnGetProductFinished(result, inv);
+            }
+        });
+    }
+
     public void launchForPayWithErrorListener(Activity activity, String s, int requestCode, IabHelper.OnIabPurchaseFinishedListener listener) {
-        if (isSetup)
-        {
+        if (isSetup) {
 
-            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener,"developer-payload");
+            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener, "developer-payload");
         }
-        else
-            cafeBazzarInterface.ErrorLaunch();
-    }
-    public void launchForPay(String s, int requestCode, IabHelper.OnIabPurchaseFinishedListener listener) {
-        if (isSetup){
-            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener,"developer-payload");
-        }
-        else
-            Log.e("CafeBazaarError","NotSetup");
+
     }
 
+    public void launchForPayWithErrorListener(Activity activity, String s, int requestCode, String extra, IabHelper.OnIabPurchaseFinishedListener listener) {
+        if (isSetup) {
 
+            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener, extra);
+        }
 
-    public void onActivityResultCafeBazaar(int requestCode, int resultCode, Intent data, OnResult onresult){
-        if (!iabHelper.handleActivityResult(requestCode, resultCode, data))
-        {
+    }
+
+    public void launchForPay(Activity activity, String s, int requestCode, IabHelper.OnIabPurchaseFinishedListener listener) {
+        if (isSetup) {
+            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener, "developer-payload");
+        } else
+            Log.e("CafeBazaarError", "NotSetup");
+    }
+
+    public void launchForPay(Activity activity, String s, int requestCode, IabHelper.OnIabPurchaseFinishedListener listener, String extra) {
+        if (isSetup) {
+            iabHelper.launchPurchaseFlow(activity, s, requestCode, listener, extra);
+        } else
+            Log.e("CafeBazaarError", "NotSetup");
+    }
+
+    public void onActivityResultCafeBazaar(int requestCode, int resultCode, Intent data, OnResult onresult) {
+        if (!iabHelper.handleActivityResult(requestCode, resultCode, data)) {
             onresult.OnResultCustom();
         } else {
             Log.d("TAG", "onActivityResult handled by IABUtil.");
         }
     }
-    public void onActivityResultCafeBazaar(Activity activity,int requestCode, int resultCode, Intent data){
-        if (!iabHelper.handleActivityResult(requestCode, resultCode, data))
-        {
-            Intent intent=new Intent(activity,activity.getClass());
-            activity.startActivityForResult(intent,requestCode);
+
+    public void onActivityResultCafeBazaar(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (!iabHelper.handleActivityResult(requestCode, resultCode, data)) {
+            Intent intent = new Intent(activity, activity.getClass());
+            activity.startActivityForResult(intent, requestCode);
         } else {
             Log.d("TAG", "onActivityResult handled by IABUtil.");
         }
+        IabHelper.OnConsumeFinishedListener listener = new IabHelper.OnConsumeFinishedListener() {
+            @Override
+            public void onConsumeFinished(Purchase purchase, IabResult result) {
+
+            }
+        };
+
     }
 
-    public void consumeProduct(Purchase purchase, IabHelper.OnConsumeFinishedListener finishedListener){
-        iabHelper.consumeAsync(purchase,finishedListener);
+    public interface OnConsumeCafeBazaarFinishedListener {
+        void OnConsumeFinished(Purchase purchase, IabResult result);
     }
+
+    public interface OnGetProductDetailFinished {
+        void OnGetProductFinished(IabResult result, Inventory inventory);
+    }
+
+    public void consumeProduct(Purchase purchase, final OnConsumeCafeBazaarFinishedListener listener) {
+
+        iabHelper.consumeAsync(purchase, new IabHelper.OnConsumeFinishedListener() {
+            @Override
+            public void onConsumeFinished(Purchase purchase, IabResult result) {
+                listener.OnConsumeFinished(purchase, result);
+            }
+        });
+
+    }
+
     @Override
     public int describeContents() {
         return 0;
     }
 
     @Override
-    public void writeToParcel(Parcel parcel, int i)
-    {
+    public void writeToParcel(Parcel parcel, int i) {
         parcel.writeByte((byte) (isSetup ? 1 : 0));
     }
 
